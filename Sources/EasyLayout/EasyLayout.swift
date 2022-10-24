fbc7db31bb11595ae303aaac45e806788af7208e
yabuki829 <syodai829@icloud.com>
2022-10-24 14:48:40 +0900

    update

--- Sources/EasyLayout/EasyLayout.swift
+++ Sources/EasyLayout/EasyLayout.swift
@@ -1,6 +1,70 @@
-public struct EasyLayout {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import UIKit
 
-    public init() {
+extension UIView{
+    func layout(top: NSLayoutYAxisAnchor? = nil,paddingTop: CGFloat = 0,
+                left: NSLayoutXAxisAnchor? = nil, paddingLeft: CGFloat = 0,
+                right: NSLayoutXAxisAnchor? = nil,paddingRight: CGFloat = 0,
+                bottom: NSLayoutYAxisAnchor? = nil,paddingBottom: CGFloat = 0) {
+    
+        self.translatesAutoresizingMaskIntoConstraints = false
+
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+
+       
+    }
+    func addSize(width: CGFloat? = nil,height: CGFloat? = nil){
+        self.translatesAutoresizingMaskIntoConstraints = false
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+
+        if let height = height{
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    func center(inView view: UIView) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+    func centerX(inView view: UIView, topAnchor: NSLayoutYAxisAnchor? = nil, paddingTop: CGFloat? = nil) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        if let topAnchor = topAnchor, let padding = paddingTop  {
+            self.topAnchor.constraint(equalTo: topAnchor, constant: padding).isActive = true
+        }
+    }
+
+    func centerY(inView view: UIView, leftAnchor: NSLayoutXAxisAnchor? = nil, paddingleft: CGFloat? = nil, constant: CGFloat? = 0) {
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant!).isActive = true
+
+        if let leftAnchor = leftAnchor, let padding = paddingleft {
+            self.leftAnchor.constraint(equalTo: leftAnchor, constant: padding).isActive = true
+        }
+    }
+    
+    func removeConstraint(){
+        removeConstraints(constraints)
     }
 }
+
+
+
+
--- Tests/EasyLayoutTests/EasyLayoutTests.swift
+++ Tests/EasyLayoutTests/EasyLayoutTests.swift
@@ -2,10 +2,5 @@ import XCTest
 @testable import EasyLayout
 
 final class EasyLayoutTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(EasyLayout().text, "Hello, World!")
-    }
+
 }
